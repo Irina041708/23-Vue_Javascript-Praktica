9e6607eb66d605aada6b585911e24fe8149b7abe
Irina041708 <q-w-i-n-86@rambler.ru>
2023-09-01 03:37:41 +0400

    DZ2_3

---
+++ DZ2_3/size_1str_2str_3str/components/comp.js
@@ -0,0 +1,358 @@
+Vue.component('change-Kitchen', {
+  
+  template: `
+  <div class="div1">
+  <div class="div1__block1">
+    <h1 class="div1__block1__h1">
+      Let’s Get Solution for Building<br />
+      Construction Work
+    </h1>
+    <a href="#" class="div1__block1__img">
+      <img
+        class="div1__block1__img__img1"
+        src="img_/3str-blog_details-section9-div1-block1-img.jpg"
+        alt="3str-blog_details-section9-div1-block1-img"
+      />
+    </a>
+    <div class="div1__block1__elem3">
+      <p class="data">26 December,2022</p>
+      <p class="menu">Interior / Home / Decore</p>
+    </div>
+    <p class="div1__block1__elem4">
+      Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+      turpmaximus.posuere in.Contrary to popular belief.There are many
+      variations of passages of Lorem Ipsum available, but the majority
+      have suffered alteration in some form, by injecthumour, or
+      randomised words which don't look even slightly believable.
+    </p>
+    <p class="div1__block1__elem5">
+      Embarrassing hidden in the middle of text. All the Lorem Ipsum
+      generators on the Internet tend to repeat predefined chunks as
+      necessary.
+    </p>
+  </div>
+  <div class="div1__block2">
+    <div class="div1__block2__background">
+      <p class="div1__block2__background__p1">
+        <img src="img_/covichki.png" alt="" />
+      </p>
+      <p class="div1__block2__background__p2">
+        The details are not the details.
+      </p>
+
+      <p class="div1__block2__background__p2 p3">They make the design.</p>
+    </div>
+  </div>
+  <div class="div1__block3">
+    <h1 class="div1__block3__h1">Design sprints are great</h1>
+    <p class="div1__block3__p1">
+      Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+      turpmaximus.posuere in.Contrary to popular belief.There are many
+      variations of passages of Lorem Ipsum available, but the majority
+      have suffered.
+    </p>
+    <ul class="div1__block3__ul">
+      <li class="li">
+        Contrary to popular belief.There are many variations of passages
+        of Lorem Ipsum available, but the majority have suffered.
+      </li>
+      <li class="li">
+        Contrary to popular belief.There are many variations of passages
+        of Lorem Ipsum available, but the majority have suffered.
+      </li>
+      <li class="li">
+        Contrary to popular belief.There are many variations of passages
+        of Lorem Ipsum available, but the majority have suffered.
+      </li>
+    </ul>
+    <a href="#" class="div1__block3__img">
+      <img
+        class="img1"
+        src="img_/3str-blog_details-section9__div1__block3__img.jpg"
+        alt="img"
+      />
+    </a>
+    <p class="div1__block3__p2">
+      Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+      turpmaximus.posuere in.Contrary to popular belief.There are many
+      variations of passages of Lorem Ipsum available, but the majority
+      have suffered.
+    </p>
+  </div>
+</div>
+`,
+});
+
+Vue.component('change-Bedroom', {
+  
+  template: `
+  <div class="div1">
+  <div class="div1__block1">
+    <h1 class="div1__block1__h1">
+    Bedroom
+    </h1>
+    <a href="#" class="div1__block1__img">
+      <img
+        class="div1__block1__img__img1"
+        src="img_/3str-blog_details-section9-div1-block1-bedroom.jpg"
+        alt="3str-blog_details-section9-div1-block1-img"
+      />
+    </a>
+    <div class="div1__block1__elem3">
+      <p class="data">26 December,2022</p>
+      <p class="menu">Interior / Home / Decore</p>
+    </div>
+    <p class="div1__block1__elem4">
+      Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+      turpmaximus.posuere in.Contrary to popular belief.There are many
+      variations of passages of Lorem Ipsum available, but the majority
+      have suffered alteration in some form, by injecthumour, or
+      randomised words which don't look even slightly believable.
+    </p>
+    <p class="div1__block1__elem5">
+      Embarrassing hidden in the middle of text. All the Lorem Ipsum
+      generators on the Internet tend to repeat predefined chunks as
+      necessary.
+    </p>
+  </div>
+  <div class="div1__block2">
+    <div class="div1__block2__background">
+      <p class="div1__block2__background__p1">
+        <img src="img_/covichki.png" alt="" />
+      </p>
+      <p class="div1__block2__background__p2">
+        The details are not the details.
+      </p>
+
+      <p class="div1__block2__background__p2 p3">They make the design.</p>
+    </div>
+  </div>
+  <div class="div1__block3">
+    <h1 class="div1__block3__h1">Design sprints are great</h1>
+    <p class="div1__block3__p1">
+      Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+      turpmaximus.posuere in.Contrary to popular belief.There are many
+      variations of passages of Lorem Ipsum available, but the majority
+      have suffered.
+    </p>
+    <ul class="div1__block3__ul">
+      <li class="li">
+        Contrary to popular belief.There are many variations of passages
+        of Lorem Ipsum available, but the majority have suffered.
+      </li>
+      <li class="li">
+        Contrary to popular belief.There are many variations of passages
+        of Lorem Ipsum available, but the majority have suffered.
+      </li>
+      <li class="li">
+        Contrary to popular belief.There are many variations of passages
+        of Lorem Ipsum available, but the majority have suffered.
+      </li>
+    </ul>
+    <a href="#" class="div1__block3__img">
+      <img
+        class="img1"
+        src="img_/3str-blog_details-section9-div1-block1-bedroom.jpg"
+        alt="img"
+      />
+    </a>
+    <p class="div1__block3__p2">
+      Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+      turpmaximus.posuere in.Contrary to popular belief.There are many
+      variations of passages of Lorem Ipsum available, but the majority
+      have suffered.
+    </p>
+  </div>
+</div>
+`,
+});
+Vue.component('change-Building', {
+  
+  template: `
+  <div class="div1">
+  <div class="div1__block1">
+    <h1 class="div1__block1__h1">
+    Building
+    </h1>
+    <a href="#" class="div1__block1__img">
+      <img
+        class="div1__block1__img__img1"
+        src="img_/3str-blog_details-section9-div1-block1-Building.jpg"
+        alt="3str-blog_details-section9-div1-block1-img"
+      />
+    </a>
+    <div class="div1__block1__elem3">
+      <p class="data">26 December,2022</p>
+      <p class="menu">Interior / Home / Decore</p>
+    </div>
+    <p class="div1__block1__elem4">
+      Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+      turpmaximus.posuere in.Contrary to popular belief.There are many
+      variations of passages of Lorem Ipsum available, but the majority
+      have suffered alteration in some form, by injecthumour, or
+      randomised words which don't look even slightly believable.
+    </p>
+    <p class="div1__block1__elem5">
+      Embarrassing hidden in the middle of text. All the Lorem Ipsum
+      generators on the Internet tend to repeat predefined chunks as
+      necessary.
+    </p>
+  </div>
+  
+</div>
+`,
+});
+Vue.component('change-Architecture', {
+  
+  template: `
+  <div class="div1">
+  <div class="div1__block1">
+    <h1 class="div1__block1__h1">
+    Architecture
+    </h1>
+    <a href="#" class="div1__block1__img">
+      <img
+        class="div1__block1__img__img1"
+        src="img_/arhitektury-3str-blog_details-section9-div1-block1-bedroom.jpg"
+        alt="3str-blog_details-section9-div1-block1-img"
+      />
+    </a>
+    <div class="div1__block1__elem3">
+      <p class="data">26 December,2022</p>
+      <p class="menu">Interior / Home / Decore</p>
+    </div>
+    <p class="div1__block1__elem4">
+      Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+      turpmaximus.posuere in.Contrary to popular belief.There are many
+      variations of passages of Lorem Ipsum available, but the majority
+      have suffered alteration in some form, by injecthumour, or
+      randomised words which don't look even slightly believable.
+    </p>
+    <p class="div1__block1__elem5">
+      Embarrassing hidden in the middle of text. All the Lorem Ipsum
+      generators on the Internet tend to repeat predefined chunks as
+      necessary.
+    </p>
+  </div>
+</div>
+`,
+});
+Vue.component('change-Kitchen Planning', {
+  
+  template: `
+  <div class="div1">
+  <div class="div1__block1">
+    <h1 class="div1__block1__h1">
+    Kitchen Planning
+    </h1>
+    <a href="#" class="div1__block1__img">
+      <img
+        class="div1__block1__img__img1"
+        src="img_/3str-blog_details-section9-div1-block1-Kitchen Planning.jpg"
+        alt="3str-blog_details-section9-div1-block1-img"
+      />
+    </a>
+    <div class="div1__block1__elem3">
+      <p class="data">26 December,2022</p>
+      <p class="menu">Interior / Home / Decore</p>
+    </div>
+    <p class="div1__block1__elem4">
+      Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+      turpmaximus.posuere in.Contrary to popular belief.There are many
+      variations of passages of Lorem Ipsum available, but the majority
+      have suffered alteration in some form, by injecthumour, or
+      randomised words which don't look even slightly believable.
+    </p>
+    <p class="div1__block1__elem5">
+      Embarrassing hidden in the middle of text. All the Lorem Ipsum
+      generators on the Internet tend to repeat predefined chunks as
+      necessary.
+    </p>
+  </div>
+  
+</div>
+`,
+});
+Vue.component('change-Bedroom2', {
+  
+  template: `
+  <div class="div1">
+  <div class="div1__block1">
+    <h1 class="div1__block1__h1">
+    Bedroom2
+    </h1>
+    <a href="#" class="div1__block1__img">
+      <img
+        class="div1__block1__img__img1"
+        src="img_/3str-blog_details-section9-div1-block1-bedroom.jpg"
+        alt="3str-blog_details-section9-div1-block1-img"
+      />
+    </a>
+    <div class="div1__block1__elem3">
+      <p class="data">26 December,2022</p>
+      <p class="menu">Interior / Home / Decore</p>
+    </div>
+    <p class="div1__block1__elem4">
+      Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+      turpmaximus.posuere in.Contrary to popular belief.There are many
+      variations of passages of Lorem Ipsum available, but the majority
+      have suffered alteration in some form, by injecthumour, or
+      randomised words which don't look even slightly believable.
+    </p>
+    <p class="div1__block1__elem5">
+      Embarrassing hidden in the middle of text. All the Lorem Ipsum
+      generators on the Internet tend to repeat predefined chunks as
+      necessary.
+    </p>
+  </div>
+  <div class="div1__block2">
+    <div class="div1__block2__background">
+      <p class="div1__block2__background__p1">
+        <img src="img_/covichki.png" alt="" />
+      </p>
+      <p class="div1__block2__background__p2">
+        The details are not the details.
+      </p>
+
+      <p class="div1__block2__background__p2 p3">They make the design.</p>
+    </div>
+  </div>
+  <div class="div1__block3">
+    <h1 class="div1__block3__h1">Design sprints are great</h1>
+    <p class="div1__block3__p1">
+      Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+      turpmaximus.posuere in.Contrary to popular belief.There are many
+      variations of passages of Lorem Ipsum available, but the majority
+      have suffered.
+    </p>
+    <ul class="div1__block3__ul">
+      <li class="li">
+        Contrary to popular belief.There are many variations of passages
+        of Lorem Ipsum available, but the majority have suffered.
+      </li>
+      <li class="li">
+        Contrary to popular belief.There are many variations of passages
+        of Lorem Ipsum available, but the majority have suffered.
+      </li>
+      <li class="li">
+        Contrary to popular belief.There are many variations of passages
+        of Lorem Ipsum available, but the majority have suffered.
+      </li>
+    </ul>
+    <a href="#" class="div1__block3__img">
+      <img
+        class="img1"
+        src="img_/3str-blog_details-section9-div1-block1-bedroom.jpg"
+        alt="img"
+      />
+    </a>
+    <p class="div1__block3__p2">
+      Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
+      turpmaximus.posuere in.Contrary to popular belief.There are many
+      variations of passages of Lorem Ipsum available, but the majority
+      have suffered.
+    </p>
+  </div>
+</div>
+`,
+});
